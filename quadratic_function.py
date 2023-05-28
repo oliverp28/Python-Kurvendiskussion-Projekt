d7dba591a9f62ce97a3b91e6caddaf9f112dc959
Stefan-Mack <116346074+Stefan-Mack@users.noreply.github.com>
2023-05-28 16:15:52 +0200

    rewrote quadratic_func, added __add__ for quadratic funcs, __repr__, __iter__ method

--- quadratic_function.py
+++ quadratic_function.py
@@ -2,57 +2,71 @@ from curve_discussion_output import Curve_Discussion_Output
 
 class Quadratic_Func:
     
-    def __init__(self, input):
+    def __init__(self, input, *, _format = True):
         """Instanciate the input from the validation class
-        
+         
         Args: 
             input (sequenz in special form): quadratic function
         """
-        entry_list = [input.split(", ")]
-            
-        while(len(entry_list) < 3): # length of a quadratic function
-            entry_list.append("0")
+        entry_list = input
 
-        for index in range(3): # removing the x's to work with the numbers
-            if "x^2" in entry_list[index] and index == 0:
-                entry_list[index] = entry_list[index].replace("x**2", "")
-                if entry_list[index] == "":
-                    entry_list = "1"
-
-            elif "x" in entry_list[index] and index == 1:
-                entry_list[index] = entry_list[index].replace("x", "")
-                if entry_list[index] == "":
-                    entry_list = "1"
-
-            elif "x" not in entry_list[index] and index < 2:
-                entry_list[index] = "0"
-
-            else:
-                entry_list[index] = entry_list[index]
-
-
-        structured_func = tuple(entry_list) 
-        try: # testing if every entry is a number
-            for func_entry in structured_func:
-                float(func_entry)
-        except ValueError:
-            raise RuntimeError("faulty input")
-            
-        self._func = structured_func
-
-    # TODO
-    #def __repr__(self):
-    #    format = ("x^2", "x", "")
-    #    output = zip(self, format)
+        if  isinstance(entry_list, (tuple, list)):
+            if len(entry_list) == 3:
+                _format = False
+                for asdf_entry in entry_list:
+                    if not isinstance(asdf_entry, (int, float)):
+                        _format = True
         
-    #   return "Quadratic_func(" + repr(output) + ")"
+        if _format:
+
+            if isinstance(input, str):
+                entry_list = input.split(", ")
+            if not isinstance (entry_list, list):
+                entry_list = list(entry_list)
+
+        
+            for index, entry in enumerate(entry_list): # removing the x's to work with the numbers
+
+                if index == 0:
+                    if isinstance(entry, str):
+                        entry_list[index] = entry.replace("x**2", "")
+                    if entry == "":
+                        entry_list[index] = "1"
+
+                elif index == 1:
+                    if isinstance(entry, str):
+                        if "x" in entry:
+                            entry_list[index] = entry.replace("x", "")
+                            if entry == "":
+                                entry_list[index] = "1"
+                            continue
+                        if len(entry_list) == 2:
+                            entry_list.insert(index, "0")
+
+                elif index == 2:
+                    entry_list[index] = entry
+
+            if len(entry_list) == 2:
+                entry_list.append(0.0)        
+
+            try: # testing if every entry is a number
+                for i, func_entry in enumerate(entry_list):
+                    entry_list[i] = float(func_entry)
+            except ValueError:
+                raise RuntimeError("faulty input")
+        self._func = tuple(entry_list)
+        print(self._func)
+
+
+    def __repr__(self): # TODO
+        return "Quadratic_func(" + repr(self._func) + ")"
     
-    # def __iter__(self):
-    #     return iter(self)
+    def __iter__(self):
+        return iter(self._func)
 
 
-    # def __add__(self, other):
-    #     if isinstance(object, valid.Validated_Func):
-    #         if(len(other) == 2):
-    #             return
-            
+    def __add__(self, other):
+        if isinstance(other, Quadratic_Func): # TODO replace Quadratic_Func with general func
+            temp = [x + y for x, y in zip (self._func, other._func)]
+            return Quadratic_Func(temp, _format = False)
+
