5a34b0b81269560b067ac46fdc85a7827ed677ef
Oliver <116276047+oliverp28@users.noreply.github.com>
2023-06-01 14:30:26 +0200

    Einfügen von Kommentaren und DocStrings

--- linear_function.py
+++ linear_function.py
@@ -1,167 +1,254 @@
-import curve_discussion_output
+from curve_discussion_output import Curve_Discussion_Output
+
 
 class Linear_Func:
     """
-        Eine Durchführung einer Kurvendisskussion einer linearen Funktion.
+    Eine Durchführung einer Kurvendisskussion einer linearen Funktion
+    Bestandteile:
+                - Ableitung
+                - Nullstelle
+                - Symmetrie zur x-Achse
+                - Symmetrie zum Ursprung
+                - Monotonie
     """
 
     def __init__(self, function):
 
         """
-            arg: 
-                -> Als Eingabe wird ein String mit einer linearen Funktion verarbeitet
-                    -> Schema der Funktion: a * x + b
-                    --> a bildet im laufe der Funktion a * x ab
-                    --> b bildet b ab (alles was nach dem x folgt)
+        arg:
+            -> Als Eingabe wird ein String mit einer linearen Funktion verarbeitet
+                -> Schema der Funktion: a * x + b
+                --> a bildet im laufe der Funktion a * x ab
+                --> b bildet b ab (alles was nach dem x folgt)
 
-                -> erlaubte Varianten: + o. - ax + o. - b
-                    --> Das * zwischen a und x kann weggelassen werden, muss aber nicht
-                    --> Abstandsregeln spielen keine Rollen
-                -> Nicht erlaubte Eingaben:
-                    --> x/a
-                    --> a/x
-                    --> ax / b
-                    --> ax * b
+            -> erlaubte Varianten: + o. - ax + o. - b
+                --> Das * zwischen a und x kann weggelassen werden, muss aber nicht
+                --> Abstandsregeln spielen keine Rollen
+            -> Nicht erlaubte Eingaben:
+                --> x/a
+                --> a/x
+                --> ax / b
+                --> ax * b
 
         """
 
-        self.function = self.validate_function(function)
+        self.function = self.validate_function(
+            function
+        )  # function, als Verwendung in Form einer Klassenvariable
 
-        curve_discussion_output.Curve_Discussion_Output(function=function,
-                                                        function_type="lin",
-                                                        derivative=self.calculate_derivative(),
-                                                        zero_lin=self.calculate_zeros(),
-                                                        symmetry_x=self.calculate_symmetry_x(),
-                                                        symmetry_origin=self.calculate_point_symmetry_origin(),
-                                                        monotonicity=self.calculate_monotonicity())
-
+        self.calculate_derivative()  # Aufruf der Funktionen
+        self.calculate_symmetry_x()
+        self.calculate_point_symmetry_origin()
+        self.calculate_monotonicity()
 
     def validate_function(self, function):
-        function = function.replace(" ", "")
-        function = function.replace(",", ".")
-        function = function.replace("*", "")
-        if function.endswith("x"):
-            function += "+0"       
+        """
+        Validierung der eingegebenen Funktion
+        Args:
+            function (str): Der String mit der linearen Funktion
+        Returns:
+            str: Die validierte Funktion
+        """
+        function = function.replace(" ", "")  # entfernen von Abständen
+        function = function.replace(",", ".")  # Standardisierung von , in .
+        function = function.replace(
+            "*", ""
+        )  # entfernen von * für Falle wie: 5*x zu 5x für bessers rechnen
+        if function.endswith(
+            "x"
+        ):  # hinzufügen von + 0 bei Funktionen wie 5x, für eine Analyse von b
+            function += "+0"
 
         return function
-    
-    def parse_function(self):  
+
+    def parse_function(self):
+        """
+        Analysieren der Funktion auf die einzelnen Bestandteile
+
+        Args:
+            function (str): Der String mit der linearen Funktion
+
+        Returns:
+            a: Funktionsteil bis einschließlich x
+            b: Funktionsteil ab x bis zum Ende
+            replace_a: Kontroll-Variable zur Analyse von seltenen Fällen
+        """
         function = self.function
-        a = ""
+        a = ""  # Instanziierung von Variablen
         b = ""
-        replace_a = ""
+        replace_x = ""  # Kontrollvariable für Funktionen wie f(x) = x
         found_x = False
-    
-        for char in function:
+
+        for char in function:  # Untersuche ob x enhalten ist
             if char == "x":
-                found_x = True
+                found_x = True  # Ist x enhalten wird der Teil bis einschließlich x in a geladen
                 a += char
                 break
             else:
                 a += char
 
         if found_x:
-            b = function[len(a):] # Anhand der Länge von a wird, alles um die Stelle 1 nach a in b geladen
+            b = function[
+                len(a) :
+            ]  # Anhand der Länge von a wird, alles um die Stelle 1 nach a in b geladen
         else:
-            b = function
+            b = function  # ist kein x vorhanden wird alles in b geladen
             a = ""
 
-        if "x" in a:
+        if (
+            "x" in a
+        ):  # x wird in a entfernt, damit mathematische Berechnungen möglich sind
             a = a.replace("x", "")
-            replace_a = True
+            replace_x = True  # diese Kontrollvariable wird bei Fallunterscheidungen miteinbezogen
         else:
-            replace_a = False
+            replace_x = False
 
-        if a == "+" or a == "-":
+        if (
+            a == "+" or a == "-"
+        ):  # ist ein x und wird für den Fall +x oder -x eine 1 für x eingesetzt
             a += "1"
-            
-        return a, b, replace_a
-    
-    def calculate_derivative(self): # a und b in einer Eingabe als Tuple!!
-        a, _, replace_a = self.parse_function()
 
-        if len(a) == 0 and replace_a == False:
+        return a, b, replace_x
+
+    def calculate_derivative(self):
+        """
+        Berechnung der Ableitung.
+
+        Args:
+            Verwendung der Bestandteile der Funktion als String in a, b und replace_a
+
+        Returns:
+            derivative: int; float; string (Datentyp abhängig von der linearen Funktion)
+        """
+        a, _, replace_x = self.parse_function()
+
+        if (
+            len(a) == 0 and replace_x is False
+        ):  # Test für den Fall dass keine Steigung vorhanden ist wie f(x) = 5
             derivative = "Keine Ableitung möglich."
             return derivative
-        
-        elif len(a) == 0 and replace_a == True:
+
+        elif (
+            len(a) == 0 and replace_x is True
+        ):  # Test für den Fall dass die Funktion f(x) = x lautet
             derivative = 1
             return derivative
-        
 
-        a = eval(a)
+        a = eval(a)  # Umrechnung von +0 oder -0 in 0
 
-        if a == 0:
+        if a == 0:  # Test für f(x) = 0x + 2
             derivative = "Keine Ableitung möglich."
         else:
             derivative = a
-        
-        return derivative 
-            
+
+        return derivative
 
     def calculate_zeros(self):
-        a, b, replace_a = self.parse_function()
-  
-        if len(a) == 0:
-         if replace_a == True and eval(b) == 0:
-              zero = 0
-              return zero 
-        
-         elif replace_a == True and eval(b) != 0:
-              zero = -eval(b) / 1
-              return zero
-         
-         elif replace_a == False:
+        """
+        Berechnung der Nullstellen.
+
+        Args:
+            Verwendung der Bestandteile der Funktion als String in a, b und replace_a
+
+        Returns:
+            zero: int; float; string (Datentyp abhängig von der linearen Funktion)
+        """
+        a, b, replace_x = self.parse_function()
+
+        if len(a) == 0:  # Test für f(x) = x
+            if replace_x is True and eval(b) == 0:
+                zero = 0
+                return zero
+
+            elif replace_x is True and eval(b) != 0:  # Test für f(x) = x + 1
+                zero = -eval(b) / 1  # Berechnung der Nullstelle mit a = 1
+                return zero
+
+            elif replace_x is False:  # Test für den Fall von keinem a
+                zero = "Keine Nullstelle vorhanden."
+                return zero
+
+        if eval(a) == 0:  # Test für den Fall f(x) = -0x + 5
             zero = "Keine Nullstelle vorhanden."
             return zero
 
-        if eval(a) == 0:
-            zero = "Keine Nullstelle vorhanden."
-            return zero
-
-        if replace_a == True and b == "0":
+        if replace_x is True and b == "0":  # Test für den Fall f(x) = 5x
             zero = 0
             return zero
 
-        if len(a) >= 1:
+        if len(a) >= 1:  # Test für den Fall f(x) = 5x + 10
             zero = -eval(b) / eval(a)
             return zero
 
     def calculate_symmetry_x(self):
-        a, b, replace_a = self.parse_function()
 
-        if a == "" or eval(a) == 0 and eval(b) != 0:
-            symmetrie_x = True
-            if a == "" and replace_a == True:
-                symmetrie_x = False
+        """
+        Feststellung der Symmetrie zur X-Achse
 
-        else:
-            symmetrie_x = False
+        Args:
+            Verwendung der Bestandteile der Funktion als String in a, b und replace_a
 
-        return symmetrie_x
-    
+        Returns:
+            symmetrie_x: bool
+        """
+
+        a, b, replace_x = self.parse_function()
+
+        if a == "" or eval(a) == 0 and eval(b) != 0:  # Test für a = 0 und b != 0
+            symmetry_x = True
+            if a == "" and replace_x is True:  # Test für a != 0x
+                symmetry_x = False
+
+        else:  # Test sobald a != 0x ist
+            symmetry_x = False
+
+        return symmetry_x
 
     def calculate_point_symmetry_origin(self):
+
+        """
+        Feststellung der Symmetrie zum Ursprung
+
+        Args:
+            Verwendung der Bestandteile der Funktion als String in b
+
+        Returns:
+            point_symmetrie_origin: bool
+        """
+
         _, b, _ = self.parse_function()
         b = eval(b)
-        if b == 0:
-            point_symmetriy_origin = True
+        if b == 0:  # Test ob b 0 entspricht
+            point_symmetry_origin = True
         else:
-            point_symmetriy_origin = False
-        
-        return point_symmetriy_origin
-    
+            point_symmetry_origin = (
+                False  # gilt für alle Fälle wenn b != 0 wie f(x) = 5x + 2
+            )
+
+        return point_symmetry_origin
 
     def calculate_monotonicity(self):
-        a, _, replace_a = self.parse_function()
-    
-        if a == "": # x wurde abgeschnitten aber der Fall f(x) = x wird hier behandelt
-            if replace_a == True:
+
+        """
+            Feststellung der Monotonie
+
+            Args:
+            Verwendung der Bestandteile der Funktion als String in b
+
+        Returns:
+            monotonicity: string
+        """
+        a, _, replace_x = self.parse_function()
+
+        if a == "":  # x wurde abgeschnitten aber der Fall f(x) = x wird hier behandelt
+            if replace_x is True:
                 monotonicity = "steigend"
             else:
-                monotonicity = "konstant"
+                monotonicity = (
+                    "konstant"  # Test des Falls dass keine Steigung in a vorhanden ist
+                )
         else:
-            a = eval(a)
+            a = eval(a)  # Test ob a a = 0 oder größer oder kleiner ist
             if a > 0:
                 monotonicity = "steigend"
             elif a < 0:
--- test_lin_func.py
+++ test_lin_func.py
@@ -1,15 +1,13 @@
 import linear_function
 
 
-
-
 def test_typical():
     test_func = "5x + 2"
     lin_function = linear_function.Linear_Func(test_func)
     assert lin_function.calculate_derivative() == 5
     assert lin_function.calculate_zeros() == -0.4
-    assert lin_function.calculate_symmetry_x() == False
-    assert lin_function.calculate_point_symmetry_origin() == False
+    assert lin_function.calculate_symmetry_x() is False
+    assert lin_function.calculate_point_symmetry_origin() is False
     assert lin_function.calculate_monotonicity() == "steigend"
 
 
@@ -18,8 +16,8 @@ def test_zero_multiply_with_positiv_end():
     lin_function = linear_function.Linear_Func(test_func)
     assert lin_function.calculate_derivative() == "Keine Ableitung möglich."
     assert lin_function.calculate_zeros() == "Keine Nullstelle vorhanden."
-    assert lin_function.calculate_symmetry_x() == True
-    assert lin_function.calculate_point_symmetry_origin() == False
+    assert lin_function.calculate_symmetry_x() is True
+    assert lin_function.calculate_point_symmetry_origin() is False
     assert lin_function.calculate_monotonicity() == "konstant"
 
 
@@ -28,18 +26,18 @@ def test_zero_multiply_with_negativ_end():
     lin_function = linear_function.Linear_Func(test_func)
     assert lin_function.calculate_derivative() == "Keine Ableitung möglich."
     assert lin_function.calculate_zeros() == "Keine Nullstelle vorhanden."
-    assert lin_function.calculate_symmetry_x() == True
-    assert lin_function.calculate_point_symmetry_origin() == False
+    assert lin_function.calculate_symmetry_x() is True
+    assert lin_function.calculate_point_symmetry_origin() is False
     assert lin_function.calculate_monotonicity() == "konstant"
-    
+
 
 def test_one_multiply_with_positive_end():
     test_func = "+ 1 * x + 1"
     lin_function = linear_function.Linear_Func(test_func)
     assert lin_function.calculate_derivative() == 1
     assert lin_function.calculate_zeros() == -1
-    assert lin_function.calculate_symmetry_x() == False
-    assert lin_function.calculate_point_symmetry_origin() == False
+    assert lin_function.calculate_symmetry_x() is False
+    assert lin_function.calculate_point_symmetry_origin() is False
     assert lin_function.calculate_monotonicity() == "steigend"
 
 
@@ -48,8 +46,8 @@ def test_one_negative_multiply_with_negative_end():
     lin_function = linear_function.Linear_Func(test_func)
     assert lin_function.calculate_derivative() == -1
     assert lin_function.calculate_zeros() == -1
-    assert lin_function.calculate_symmetry_x() == False
-    assert lin_function.calculate_point_symmetry_origin() == False
+    assert lin_function.calculate_symmetry_x() is False
+    assert lin_function.calculate_point_symmetry_origin() is False
     assert lin_function.calculate_monotonicity() == "fallend"
 
 
@@ -58,8 +56,8 @@ def test_one_negative_multiply_with_0_end():
     lin_function = linear_function.Linear_Func(test_func)
     assert lin_function.calculate_derivative() == -1
     assert lin_function.calculate_zeros() == 0
-    assert lin_function.calculate_symmetry_x() == False
-    assert lin_function.calculate_point_symmetry_origin() == True
+    assert lin_function.calculate_symmetry_x() is False
+    assert lin_function.calculate_point_symmetry_origin() is True
     assert lin_function.calculate_monotonicity() == "fallend"
 
 
@@ -68,8 +66,8 @@ def test_only_x():
     lin_function = linear_function.Linear_Func(test_func)
     assert lin_function.calculate_derivative() == 1
     assert lin_function.calculate_zeros() == 0
-    assert lin_function.calculate_symmetry_x() == False
-    assert lin_function.calculate_point_symmetry_origin() == True
+    assert lin_function.calculate_symmetry_x() is False
+    assert lin_function.calculate_point_symmetry_origin() is True
     assert lin_function.calculate_monotonicity() == "steigend"
 
 
@@ -78,8 +76,8 @@ def test_only_negative_x():
     lin_function = linear_function.Linear_Func(test_func)
     assert lin_function.calculate_derivative() == -1
     assert lin_function.calculate_zeros() == 0
-    assert lin_function.calculate_symmetry_x() == False
-    assert lin_function.calculate_point_symmetry_origin() == True
+    assert lin_function.calculate_symmetry_x() is False
+    assert lin_function.calculate_point_symmetry_origin() is True
     assert lin_function.calculate_monotonicity() == "fallend"
 
 
@@ -88,8 +86,8 @@ def test_only_one():
     lin_function = linear_function.Linear_Func(test_func)
     assert lin_function.calculate_derivative() == "Keine Ableitung möglich."
     assert lin_function.calculate_zeros() == "Keine Nullstelle vorhanden."
-    assert lin_function.calculate_symmetry_x() == True
-    assert lin_function.calculate_point_symmetry_origin() == False
+    assert lin_function.calculate_symmetry_x() is True
+    assert lin_function.calculate_point_symmetry_origin() is False
     assert lin_function.calculate_monotonicity() == "konstant"
 
 
@@ -98,8 +96,8 @@ def test_decimal_num_with_different_signs():
     lin_function = linear_function.Linear_Func(test_func)
     assert lin_function.calculate_derivative() == 0.5
     assert lin_function.calculate_zeros() == -2
-    assert lin_function.calculate_symmetry_x() == False
-    assert lin_function.calculate_point_symmetry_origin() == False
+    assert lin_function.calculate_symmetry_x() is False
+    assert lin_function.calculate_point_symmetry_origin() is False
     assert lin_function.calculate_monotonicity() == "steigend"
 
 
@@ -108,8 +106,8 @@ def test_only_decimal_num():
     lin_function = linear_function.Linear_Func(test_func)
     assert lin_function.calculate_derivative() == "Keine Ableitung möglich."
     assert lin_function.calculate_zeros() == "Keine Nullstelle vorhanden."
-    assert lin_function.calculate_symmetry_x() == True
-    assert lin_function.calculate_point_symmetry_origin() == False
+    assert lin_function.calculate_symmetry_x() is True
+    assert lin_function.calculate_point_symmetry_origin() is False
     assert lin_function.calculate_monotonicity() == "konstant"
 
 
@@ -118,7 +116,16 @@ def test_high_value():
     lin_function = linear_function.Linear_Func(test_func)
     assert lin_function.calculate_derivative() == 100
     assert lin_function.calculate_zeros() == -5
-    assert lin_function.calculate_symmetry_x() == False
-    assert lin_function.calculate_point_symmetry_origin() == False
+    assert lin_function.calculate_symmetry_x() is False
+    assert lin_function.calculate_point_symmetry_origin() is False
     assert lin_function.calculate_monotonicity() == "steigend"
 
+
+def test_only_x_with_end():
+    test_func = "x + 2"
+    lin_function = linear_function.Linear_Func(test_func)
+    assert lin_function.calculate_derivative() == 1
+    assert lin_function.calculate_zeros() == -2
+    assert lin_function.calculate_symmetry_x() is False
+    assert lin_function.calculate_point_symmetry_origin() is False
+    assert lin_function.calculate_monotonicity() == "steigend"
