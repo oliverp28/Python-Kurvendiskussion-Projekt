2e500fd360c856a13289d6f9a5c50b2f76e941ab
Stefan-Mack <116346074+Stefan-Mack@users.noreply.github.com>
2023-06-03 12:49:42 +0200

    changes for pylint

--- test_quad_func.py
+++ test_quad_func.py
@@ -1,98 +1,137 @@
-from quadratic import Quadratic_Func
+"""test cases for the Quadratic_Func class"""
 import pytest
+from quadratic import Quadratic_Func
 
 
-# TODO: pos neg
+G1 = "1x**2"  # touchpoint
+G2 = "1x**2, 2x, -3"  # 2 intersections
+G3 = "1x**2, 5"  # no intersection
+G4 = "3x**2, 4x"
+G5 = (1, 2, -3)
+G6 = "1x**2, 2x, 3, 4"  # to much entries -> should raise an error
+G7 = "-1x**2"
 
-g1 = "1x**2" # touchpoint
-g2 = "1x**2, 2x, -3" # 2 intersections
-g3 = "1x**2, 5" # no intersection
-g4 = "3x**2, 4x"
-g5 = (1, 2, -3)
-g6 = "1x**2, 2x, 3, 4" # to much entries -> should raise an error
-g7 = "-1x**2"
+NR1 = 1.0
+NR2 = 2.0
+NR3 = 5.0
 
-nr1 = 1.0
-nr2 = 2.0
-nr3 = 5.0
+g1_as_func = Quadratic_Func(G1)
+g2_as_func = Quadratic_Func(G2)
+g3_as_func = Quadratic_Func(G3)
+g4_as_func = Quadratic_Func(G4)
+g5_as_func = Quadratic_Func(G5)
+g7_as_func = Quadratic_Func(G7)
 
-g1_as_func = Quadratic_Func(g1)
-g2_as_func = Quadratic_Func(g2)
-g3_as_func = Quadratic_Func(g3)
-g4_as_func = Quadratic_Func(g4)
-g5_as_func = Quadratic_Func(g5)
-g7_as_func = Quadratic_Func(g7)
 
 def test_input_validation_to_much_entries():
-    with pytest.raises(RuntimeError): 
-        Quadratic_Func(g6)
+    """testing instanciation with wrong input"""
+    with pytest.raises(RuntimeError):
+        Quadratic_Func(G6)
 
 
 def test_input_validation_one_entry():
+    """testing instanciation with only x**2"""
     assert g1_as_func is not None
     assert g1_as_func == Quadratic_Func((1.0, 0.0, 0.0))
 
+
 def test_input_validation_two_entries():
+    """testing instanciation if an object returns with two entries"""
     assert g3_as_func is not None
     assert g4_as_func is not None
 
+
 def test_input_validation_three_entries():
+    """testing instanciation if an object returns with three entries"""
     assert g2_as_func is not None
 
 
 def test_contains():
-    assert nr1 in g1_as_func 
-    assert nr2 in g2_as_func 
-    assert nr3 in g3_as_func
+    """testing the in-operator"""
+    assert NR1 in g1_as_func
+    assert NR2 in g2_as_func
+    assert NR3 in g3_as_func
+
 
 def test_contains_not():
-    assert nr2 not in g1_as_func
-    assert nr3 not in g2_as_func
-    assert nr1 not in g4_as_func
+    """testing the in operator in negative way"""
+    assert NR2 not in g1_as_func
+    assert NR3 not in g2_as_func
+    assert NR1 not in g4_as_func
+
 
 def test_pos():
+    """testing the plus operator for positiv"""
     assert +g1_as_func == g1_as_func
 
+
 def test_neg():
-    assert -g1_as_func == g7_as_func # g7 is the negative function of g1
+    """testing the minus operator for negativ results"""
+    assert -g1_as_func == g7_as_func  # g7 is the negative function of g1
+
 
 def test_add_two_function_same_length():
-    assert (g2_as_func + g2_as_func) == Quadratic_Func((2.0, 4.0, -6.0)) 
+    """testing plus with two functions with the same length"""
+    assert (g2_as_func + g2_as_func) == Quadratic_Func((2.0, 4.0, -6.0))
+
 
 def test_add_two_function_first_is_shorter():
+    """testing plus with two functions, when the first function is shorter"""
     assert (g1_as_func + g2_as_func) == Quadratic_Func((2.0, 2.0, -3.0))
 
+
 def test_add_two_function_second_is_shorter():
+    """testing plus with two functions, when the second functions is shorter"""
     assert (g3_as_func + g1_as_func) == Quadratic_Func((2.0, 0.0, 5.0))
 
 
 def test_sub_two_function_same_length():
-    assert (g2_as_func - g2_as_func) == Quadratic_Func((0.0, 0.0, 0.0)) 
+    """testing minus with two functions with the same length"""
+    assert (g2_as_func - g2_as_func) == Quadratic_Func((0.0, 0.0, 0.0))
+
 
 def test_sub_two_function_first_is_shorter():
+    """testing minus with two functions, when the first one is shorter"""
     assert (g1_as_func - g2_as_func) == Quadratic_Func((0.0, -2.0, 3.0))
 
+
 def test_sub_two_function_second_is_shorter():
+    """testing minus with two functions, when the second one is shorter"""
     assert (g3_as_func - g1_as_func) == Quadratic_Func((0.0, 0.0, 5.0))
 
 
-y_axis_shift = 5
-def test_add_number(): # known as y-axis-shift in negative in positive direction
-    assert (g1_as_func + y_axis_shift) == Quadratic_Func((1.0, 0.0, 5.0))
-    assert (g2_as_func + y_axis_shift) == Quadratic_Func((1.0, 2.0, 2.0))
-    assert (g3_as_func + y_axis_shift) == Quadratic_Func((1.0, 0.0, 10.0))
-
-def test_sub_number(): # known as y-axis-shift in negative direction
-    assert (g1_as_func - y_axis_shift) == Quadratic_Func((1.0, 0.0, -5.0))
-    assert (g2_as_func - y_axis_shift) == Quadratic_Func((1.0, 2.0, -8.0))
-    assert (g3_as_func - y_axis_shift) == Quadratic_Func((1.0, 0.0, 0.0))
+Y_AXIS_SHIFT = 5
 
 
-def test_nullstelle_berechnen_2_Schnittpunkte():
+def test_add_number():  # known as y-axis-shift in negative in positive direction
+    """testing add with a function and a number"""
+    assert (g1_as_func + Y_AXIS_SHIFT) == Quadratic_Func((1.0, 0.0, 5.0))
+    assert (g2_as_func + Y_AXIS_SHIFT) == Quadratic_Func((1.0, 2.0, 2.0))
+    assert (g3_as_func + Y_AXIS_SHIFT) == Quadratic_Func((1.0, 0.0, 10.0))
+
+
+def test_sub_number():  # known as y-axis-shift in negative direction
+    """testing subtraction with a function and a number"""
+    assert (g1_as_func - Y_AXIS_SHIFT) == Quadratic_Func((1.0, 0.0, -5.0))
+    assert (g2_as_func - Y_AXIS_SHIFT) == Quadratic_Func((1.0, 2.0, -8.0))
+    assert (g3_as_func - Y_AXIS_SHIFT) == Quadratic_Func((1.0, 0.0, 0.0))
+
+
+def test_zeropoints_with_two_intersections():
+    """testing the zeropoints function with two zeropoints"""
     assert g2_as_func.zeropoints == (1.0, -3.0)
 
-def test_nullstelle_berechnen_Beruehrpunkt():
+
+def test_zeropoints_with_one_intersection():
+    """testing the zeropoints function with one zeropoint"""
     assert g1_as_func.zeropoints == 0.0
 
-def test_nullstelle_berechnen_kein_Schnittpunkt():
-    assert g3_as_func.zeropoints == None
+
+def test_zeropoints_with_no_intersection():
+    """testing the zeropoints function with no zeropoint"""
+    assert g3_as_func.zeropoints is None
+
+
+def test_y_intercept():
+    """testing the y_intercept function"""
+    assert g2_as_func.y_intercept == -3.0
