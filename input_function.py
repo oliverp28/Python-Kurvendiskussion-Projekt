b2d62efb4836d2f7151908341aefc04eb480b6c6
d4v1d90 <116561722+d4v1d90@users.noreply.github.com>
2023-06-07 18:43:28 +0200

    Update input_function.py

--- input_function.py
+++ input_function.py
@@ -4,19 +4,43 @@ from quadratic_function import Quadratic_Function
 class Input_Function:
     def __init__(self, function):
         """
-        Get and validate input function
+        Initialize Input_Function object.
+
+        Args:
+            function (str): The input function to validate.
+
+        Raises:
+            ValueError: If the input function is invalid.
         """
         self.validate_input(function)  # Validate the input function
         self.which_function(function)  # Determine the type of function
 
     def validate_input(self, function):
+        """
+        Validate the input function.
+
+        Args:
+            function (str): The input function to validate.
+
+        Raises:
+            ValueError: If the input function is invalid.
+        """
         # Check for invalid cases
         invalid_cases = ["/x", "x/", "*x", "x*", "^x", "x^"]
         for case in invalid_cases:
             if case in function:
-                raise ValueError("Invalid input. Please use the correct format: 1/5x; 5/2x; 25x")
+                raise ValueError("Invalid input " + case)
 
     def validate_linear_function(self, function):
+        """
+        Validate a linear function.
+
+        Args:
+            function (str): The input function to validate.
+
+        Raises:
+            ValueError: If the linear function is invalid.
+        """
         # Check for invalid cases in linear function
         if "x^" in function or "^x" in function:
             raise ValueError("Invalid input. Linear functions should not contain '^'")
@@ -25,12 +49,21 @@ class Input_Function:
             raise ValueError("Invalid input. Linear functions should have only one 'x' term")
 
         if any(symbol.isdigit() for symbol in function):
-            raise ValueError("Invalid input. Symbol numbers are not allowed. Please use the correct format: 1/5x; 5/2x; 25x")
+            raise ValueError("Invalid input. Symbol numbers are not allowed. Please use the correct format.)
 
         if "x" not in function:
             raise ValueError("Invalid input. Linear functions should contain the term 'x'")
 
     def validate_quadratic_function(self, function):
+        """
+        Validate a quadratic function.
+
+        Args:
+            function (str): The input function to validate.
+
+        Raises:
+            ValueError: If the quadratic function is invalid.
+        """
         # Check for invalid cases in quadratic function
         if function.count("x^2") > 1:
             raise ValueError("Invalid input. Quadratic functions should have only one 'x^2' term")
@@ -39,12 +72,21 @@ class Input_Function:
             raise ValueError("Invalid input. Quadratic functions should have at most two 'x' terms")
 
         if any(symbol.isdigit() for symbol in function):
-            raise ValueError("Invalid input. Symbol numbers are not allowed. Please use the correct format: 1/5x^2; 5/2x^2 + 3x; 25x^2 - 2x + 1")
+            raise ValueError("Invalid input. Symbol numbers are not allowed. Please use the correct format.")
 
         if "x^2" not in function:
             raise ValueError("Invalid input. Quadratic functions should contain the term 'x^2'")
 
     def which_function(self, function):
+        """
+        Determine the type of function.
+
+        Args:
+            function (str): The input function to analyze.
+
+        Raises:
+            ValueError: If the input function is not a valid function.
+        """
         # Remove whitespace from the input function
         function = function.replace(" ", "")
 
@@ -67,12 +109,14 @@ class Input_Function:
                 Linear_Function(function.lower())  # Create a Linear_Function instance with lowercase x
             else:
                 # If none of the conditions above are met, it's an invalid input for a function
-                raise ValueError("This input is incorrect. Please specify a valid function")
+                raise ValueError("Invalid input. Please specify a valid function")
         except ValueError as e:
             print(e)
 
-
 def validation():
+    """
+    Validate the input function provided by the user.
+    """
     try:
         # Get the function from the user
         function = input("f(x) = ")
@@ -81,6 +125,5 @@ def validation():
         input_func = Input_Function(function)
     except ValueError as e:
         print(e)
-
 
 validation()
